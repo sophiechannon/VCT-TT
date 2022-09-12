5615476d37d2c033124bdcdde233466ad43ce7ef
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-09-12 11:06:00 +0100

    adds records controller to express server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const recordsRouter = require("./controller/records");
+
+app.use("/records", recordsRouter);
+
 app.get("/", (req, res) => {
   res.send({ express: "Backend connected to React" });
 });
